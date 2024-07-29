2e9294acb58c908da684f5a6124b0269e5bcc84b
James Woglom <j@wogloms.net>
2024-07-28 22:28:24 -0400

    stupid heuristics

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import json
 import time
 import requests
 from lxml import etree as ET
+import re
 
 from flask import Flask, Response, request, abort, redirect, jsonify
 
@@ -20,40 +21,48 @@ logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
 MAX_ITEMS = 50
-def proxy(path, max_items=MAX_ITEMS):
+def proxy(path, max_items=MAX_ITEMS, mode=None):
     logger.info('proxy(%s): start' % path)
     r = requests.get(path, timeout=10)
     logger.info('proxy(%s): fetched %d' % (path, r.status_code))
-    text = r.text.encode('utf-8')
-    root = ET.fromstring(text)
-    logger.info('proxy(%s): parsed len=%d ln=%d' % (path, len(text), len(root[0])))
+    text = r.text
 
-    item_count = 0
-    i = 0
-    ln = len(root[0])
-    while i < ln:
-        if root[0][i].tag == 'item':
-            item_count += 1
-            if item_count >= max_items:
-                del root[0][i]
-                ln = len(root[0])
+    logger.info('proxy(%s): mode %s' % (path, mode))
+    if mode == 'lxml':
+        root = ET.fromstring(text.encode('utf-8'))
+        logger.info('proxy(%s): parsed len=%d ln=%d' % (path, len(text), len(root[0])))
+
+        item_count = 0
+        i = 0
+        ln = len(root[0])
+        while i < ln:
+            if root[0][i].tag == 'item':
+                item_count += 1
+                if item_count >= max_items:
+                    del root[0][i]
+                    ln = len(root[0])
+                else:
+                    i += 1
             else:
                 i += 1
-        else:
-            i += 1
 
 
-    text = ET.tostring(root)
+        text = ET.tostring(root)
+    elif mode == 'fast':
+        indexes = [m.start() for m in re.finditer(r"<item>", text)]
+        if len(indexes) > max_items:
+            text = text[:indexes[max_items]] + "</channel></rss>"
+
     logger.info('proxy(%s): done' % path)
     return Response(text, mimetype='application/xml; charset=utf-8')
 
 @app.route('/verge')
 def verge():
-    return proxy('https://www.theverge.com/rss/full.xml')
+    return proxy('https://www.theverge.com/rss/full.xml', mode='lxml')
 
 @app.route('/daily')
 def daily():
-    return proxy('https://feeds.simplecast.com/54nAGcIl')
+    return proxy('https://feeds.simplecast.com/54nAGcIl', mode='fast')
 
 @app.route('/healthz')
 def healthz_route():
